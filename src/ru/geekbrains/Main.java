19b57a910e457e378e52cd5596de918f3e271ac5
Yury Kropotkin <yury.a.kropotkin@gmail.com>
2020-07-14 09:47:25 +1100

    Задание 3 вывод на консоль

--- src/ru/geekbrains/Main.java
+++ src/ru/geekbrains/Main.java
@@ -6,16 +6,23 @@ public class Main {
     int anInt = 0;
     long aLong = 0;
     float aFloat = 0f;
-    double aDouble = 0.0;
+    static double aDouble = 0.0;
     boolean aBoolean = false;
     char aChar = 'a';
 
     String string = "Эта текстовая строка"; // or null для ссылочных типов
 
-
+    public static double doMath (double a, double b, double c, double d) {
+       if (d != 0) {
+           return a * (b + (c / d));
+       }
+        return 0;
+    }
 
 
     public static void main(String[] args) {
 	// write your code here
+        aDouble = doMath(10,20,19,30);
+        System.out.print(aDouble);
     }
 }
