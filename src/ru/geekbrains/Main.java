7d031e3d360831e59015a70b53ed5b0e67866c40
Yury Kropotkin <yury.a.kropotkin@gmail.com>
2020-07-14 09:34:14 +1100

    Задание 2

--- src/ru/geekbrains/Main.java
+++ src/ru/geekbrains/Main.java
@@ -1,6 +1,19 @@
 package ru.geekbrains;
 
 public class Main {
+    byte aByte = 0;
+    short aShort = 0;
+    int anInt = 0;
+    long aLong = 0;
+    float aFloat = 0f;
+    double aDouble = 0.0;
+    boolean aBoolean = false;
+    char aChar = 'a';
+
+    String string = "Эта текстовая строка"; // or null для ссылочных типов
+
+
+
 
     public static void main(String[] args) {
 	// write your code here
