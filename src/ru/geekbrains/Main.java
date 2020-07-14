bb9dce3de650a5f1a60974cbe5cbdc7cc8a17e01
Yury Kropotkin <yury.a.kropotkin@gmail.com>
2020-07-14 12:41:32 +1100

    Добавляем метод checkLimit принимающий на вход два числа, и проверяющий что их сумма лежит в пределах от 10 до 20(включительно) с выводом результа на консоль

--- src/ru/geekbrains/Main.java
+++ src/ru/geekbrains/Main.java
@@ -3,9 +3,15 @@ package ru.geekbrains;
 import java.util.Scanner;
 
 public class Main {
+
+//1. Создать пустой проект в IntelliJ IDEA и прописать метод main();
+//2. Создать переменные всех пройденных типов данных, и инициализировать их значения;
+//3. Написать метод вычисляющий выражение a * (b + (c / d)) и возвращающий результат,где a, b, c, d –
+//    входные параметры этого метода;
+
     byte aByte = 0;
     short aShort = 0;
-    int anInt = 0;
+    static int anInt = 0;
     long aLong = 0;
     float aFloat = 0f;
     static double aDouble = 0.0;
@@ -14,7 +20,7 @@ public class Main {
 
     String string = "Эта текстовая строка"; // or null для ссылочных типов
 
-    public static double doMath (double a, double b, double c, double d) {
+    private static double doMath (double a, double b, double c, double d) {
        if (d != 0) {
            return a * (b + (c / d));
        } else {
@@ -25,9 +31,60 @@ public class Main {
     }
 
     private static boolean checkLimit (int first, int second) {
-        int summ = first + second;
-        return summ >= 10 & summ <= 20;
+        int s = first + second;
+        return s >= 10 & s <= 20;
     }
+
+    /*
+     *  Написать метод, которому в качестве параметра передается целое число,
+     * метод должен напечатать в консоль положительное ли число передали,
+     * или отрицательное; Замечание: ноль считаем положительным числом.
+     */
+    private static void doRetPositiveOrNegative (int date) {
+        if (date >= 0 ) {
+            System.out.println("Введенное число "+date+" является положительным");
+        } else {
+            System.out.println("Введенное число "+date+" является отрицательным");
+        }
+
+     }
+
+     /*
+      *  6. Написать метод, которому в качестве параметра передается целое число,
+      *  метод должен вернуть true, если число отрицательное;
+      */
+
+    private static boolean isNegative (double data) {
+        return data < 0;
+    }
+
+    /*
+     * 7. Написать метод, которому в качестве параметра передается строка, обозначающая имя,
+     *    метод должен вывести в консоль сообщение «Привет, указанное_имя!»;
+     */
+    private static void getName (String name) {
+        System.out.println("Привет, " + name);
+    }
+
+    /*
+     * 8. * Написать метод, который определяет является ли год високосным, и выводит сообщение в консоль.
+     * Каждый 4-й год является високосным, кроме каждого 100-го,
+     * при этом каждый 400-й – високосный.
+     */
+    private static void isLeapYear (int year) {
+
+
+        if (year % 4 != 0) {
+            System.out.println("Год "+year+" не является високосным");
+        } else {
+            if (year % 100 == 0) {
+                if (year % 400 == 0) System.out.println(year +" високосный год,");
+                else  System.out.println("Год "+year+" не является високосным");
+            } else  System.out.println(year +" високосный год,");
+       }
+    }
+
+
 
 
     public static void main(String[] args) {
@@ -39,18 +96,17 @@ public class Main {
         int first = 0;
         int second = 0;
         System.out.println("Введите значения: ");
-
         Scanner scanner = new Scanner(System.in);
-        System.out.println("Введите первый параметр a: " );
+        System.out.println("Введите первое число a: " );
         a = scanner.nextDouble();
-        System.out.println("Введите второй параметр b: " );
+        System.out.println("Введите второе число b: " );
         b = scanner.nextDouble();
-        System.out.println("Введите третий параметр c: " );
+        System.out.println("Введите третье число c: " );
         c = scanner.nextDouble();
-        System.out.println("Введите четвертый параметр d: " );
+        System.out.println("Введите четвертое число d: " );
         d = scanner.nextDouble();
-        aDouble = doMath(a, b, c, d);
-        System.out.println ("Результат вычисления a * (b + (c / d)): " + aDouble);
+        Main.aDouble = doMath(a, b, c, d);
+        System.out.println ("Результат вычисления a * (b + (c / d)): " + Main.aDouble);
         System.out.println();
         System.out.println("===================================================");
         System.out.println();
@@ -63,5 +119,27 @@ public class Main {
                 "диапазоне 10 <= sum <= 20 " + checkLimit(first, second) );
         else System.out.println("Сумма введенных чисел не находиться в " +
                 "диапазоне 10 <= sum <= 20 " + checkLimit(first, second) );
+        // проверим введенное числа на положительное или отрицательное
+        System.out.println();
+        System.out.println("===================================================");
+        System.out.println();
+        System.out.println("Введите целое число: ");
+        anInt = scanner.nextInt();
+        doRetPositiveOrNegative(anInt);
+
+//        Проверим является ли число отрицательным
+        System.out.println("Введите число, если число < 0 метод вернет true, иначе false ");
+        double n = scanner.nextDouble();
+        System.out.println(isNegative(n));
+
+        // Приветствие:
+        System.out.println("Напишите свое имя!");
+        getName(scanner.next());
+
+        System.out.println("Введите год в формате YYYY, например 2020");
+        anInt = scanner.nextInt();
+        isLeapYear (anInt);
+
     }
+
 }
